d3b5c27fa711ac3ed6938a9b0af3207a56a12112
zugzw4ng <bongcloud2001@gmail.com>
2021-11-05 15:47:21 -0400

    fixed service worker, offline functioning

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,35 +1,36 @@
-const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/index.js",
-    "/db.js",
-    "/styles.css",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png",
-    "/manifest.webmanifest"
-];
-
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/db.js',
+    '/index.js',
+    '/manifest.webmanifest',
+    '/styles.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+];
+
 // install
-self.addEventListener("install", (evt) => {
+self.addEventListener("install", function (evt) {
     evt.waitUntil(
         caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
     );
-    evt.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-    );
 
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    );
     self.skipWaiting();
 });
-
 // activate
-self.addEventListener("activate", (evt) => {
+self.addEventListener("activate", function (evt) {
     evt.waitUntil(
-        caches.keys().then((keyList) => {
+        caches.keys().then(keyList => {
             return Promise.all(
-                keyList.map((key) => {
+                keyList.map(key => {
                     if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
                         console.log("Removing old cache data", key);
                         return caches.delete(key);
@@ -41,7 +42,6 @@ self.addEventListener("activate", (evt) => {
 
     self.clients.claim();
 });
-
 // fetch
 self.addEventListener("fetch", function (evt) {
     if (evt.request.url.includes("/api/")) {
@@ -52,6 +52,7 @@ self.addEventListener("fetch", function (evt) {
                         if (response.status === 200) {
                             cache.put(evt.request.url, response.clone());
                         }
+
                         return response;
                     })
                     .catch(err => {
@@ -59,6 +60,7 @@ self.addEventListener("fetch", function (evt) {
                     });
             }).catch(err => console.log(err))
         );
+
         return;
     }
     evt.respondWith(
