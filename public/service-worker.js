f75ea524d74c6d17fdeebb0c8417e0b42aa57420
zugzw4ng <bongcloud2001@gmail.com>
2021-11-05 14:37:36 -0400

    updated FILES TO CACHE to match file structure

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,13 @@
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/favorites.html',
-    '/topic.html',
-    '/assets/css/style.css',
-    '/dist/app.bundle.js',
-    '/dist/favorites.bundle.js',
-    '/dist/topic.bundle.js',
-    'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
-    'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
-  ];
+    './',
+    './index.html',
+    './db.js',
+    './index.js',
+    './manifest.webmanifest',
+    './style.css',
+    './icons/icon-192x192.png',
+    './icons/icon-512x512.png',
+];
   
   const PRECACHE = 'precache-v1';
   const RUNTIME = 'runtime';
